6ad8fbd3c88cfff5c2496fa5a2ac6e18772638be
totegsito <pacoquesmun@gmail.com>
2020-12-01 19:49:12 +0100

    dia 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # advent-of-code-2020
+
+Soluciones para el reto de Advent of code 2020. 
+
+| Día   | Solución |
+| ----- | -------- |
+|   1   | [Día 1](src/day1/index.js) |
+
---
+++ src/day1/index.js
@@ -0,0 +1,23 @@
+const data = require('./input.json')
+
+let encontrado = false;
+let product = 0;
+
+for (let i = 0; i < data.length - 1 && !encontrado; i += 1) {
+    const secondArray = data.slice(i + 1)
+    for (let j = 0; j < secondArray.length && !encontrado; j += 1) {
+        const secondSum = data[i] + secondArray[j];
+        if (secondSum <= 2020) {
+            const thirdArray = secondArray.slice(j + 1);
+            for (let k = 0; k < thirdArray.length && !encontrado; k += 1) {
+                if (secondSum + thirdArray[k] === 2020) {
+                    encontrado = true;
+                    product = data[i] * secondArray[j] * thirdArray[k];
+                }
+            }
+
+        }
+    }
+}
+
+console.log(product)
